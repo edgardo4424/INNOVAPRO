02421070d53ec37b17b1ac66e152fa2a4f005051
Andres <aemzwtf@gmail.com>
2025-05-19 12:40:03 -0500

    (bugfix): Render de tabla de cotizaciones y corrección en tipo de cotización

--- frontend/src/modules/cotizaciones/components/TablaCotizacion.jsx
+++ frontend/src/modules/cotizaciones/components/TablaCotizacion.jsx
@@ -1,5 +1,4 @@
 export default function TablaCotizacion({ cotizaciones, onDownloadPDF }) {
-
   return (
     <div className="table-responsive">
       <table className="custom-table">
@@ -7,36 +6,36 @@ export default function TablaCotizacion({ cotizaciones, onDownloadPDF }) {
           <tr>
             <th>Razón Social</th>
             <th>Contacto</th>
-            <th>Tipo </th>
-            <th>Obra </th>
+            <th>Tipo</th>
+            <th>Obra</th>
             <th>Comercial</th>
             <th>Acciones</th>
           </tr>
         </thead>
         <tbody>
-        {cotizaciones.map((cotizacion, index) => (
-          <tr key={cotizacion.id}>
-            <td>{cotizacion.cliente.razon_social || "—"}</td>
-            <td>{cotizacion.contacto.nombre || "—"}</td>
-            <td>{cotizacion.tipo_cotizacion || "—"}</td>
-            <td>{cotizacion.obra.nombre || "—"}</td>
-            <td>{cotizacion.usuario.nombre || "—"}</td>
-            <td>
-              <div
-                style={{ display: "flex", gap: "1px", justifyContent: "left" }}
-              >
-                <button
-                  onClick={() => onDownloadPDF(cotizacion)}
-                  className="edit-button"
+          {cotizaciones?.map((cotizacion, index) => (
+            <tr key={cotizacion.id || index}>
+              <td>{cotizacion.cliente?.razon_social || "—"}</td>
+              <td>{cotizacion.contacto?.nombre || "—"}</td>
+              <td>{cotizacion.tipo_cotizacion || "—"}</td>
+              <td>{cotizacion.obra?.nombre || "—"}</td>
+              <td>{cotizacion.usuario?.nombre || "—"}</td>
+              <td>
+                <div
+                  style={{ display: "flex", gap: "1px", justifyContent: "left" }}
                 >
-                 PDF
-                </button>
-                <button className="edit-button">✏️Editar</button>
-                <button className="btn-eliminar">🗑Eliminar</button>
-              </div>
-            </td>
-          </tr>
-           ))}
+                  <button
+                    onClick={() => onDownloadPDF(cotizacion)}
+                    className="edit-button"
+                  >
+                    PDF
+                  </button>
+                  <button className="edit-button">✏️Editar</button>
+                  <button className="btn-eliminar">🗑Eliminar</button>
+                </div>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
