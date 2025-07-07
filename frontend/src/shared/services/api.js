0666644646db3b5e6d07ed18daf140d46890210b
Dev-Simon20 <jeanpierks6@gmail.com>
2025-07-07 08:12:04 -0500

    Se configuro interceptor de Axios para cerrar sesión si expira el token

--- frontend/src/shared/services/api.js
+++ frontend/src/shared/services/api.js
@@ -37,7 +37,7 @@ api.interceptors.request.use((config) => {
 api.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response?.status === 401) {
+    if (error.response?.status === 417) {
       console.warn("⚠️ Sesión expirada. Redirigiendo al login...");
       localStorage.removeItem("token");
       localStorage.removeItem("user");
