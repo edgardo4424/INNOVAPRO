89b61eb8d04279e3593be7c91fe3e543e2412f67
devSimon20 <jeanpierks6@gmail.com>
2025-10-01 07:53:57 -0500

    Fix: Update cargo IDs and comments for role "Gerente de administración" and add missing case for "Jefe TI"

--- backend/src/modules/trabajadores/infraestructure/utils/obtenerCargosArea.js
+++ backend/src/modules/trabajadores/infraestructure/utils/obtenerCargosArea.js
@@ -5,25 +5,34 @@ const obtenerCargosArea = (rol) => {
   switch (rol) {
     case "Gerente de administración":
       obj.area_id = 1;
-      obj.cargos_id = [2];
-      // Area:administracion
+      obj.cargos_id = [2,13];
+      // Area:Administracion,Legal,Marketing
       // Cargos: gerente de admin, abogada
       break;
     case "Gerente de comercialización":
       obj.area_id = 9;
       obj.cargos_id = [3];
+      //Areas: ventas
       break;
     case "Jefe de OT":
       obj.area_id = 7;
       obj.cargos_id = [];
+      //Areas: ot 
       break;
     case "Jefa de Almacén":
       obj.area_id = 2;
       obj.cargos_id = [];
+      //Almacen
       break;
     case "Jefe montadores y operadores":
       obj.area_id = 6;
       obj.cargos_id = [];
+      //Montadores
+      break;
+    case "Jefe TI":
+      obj.area_id = 8;
+      obj.cargos_id = [];
+      //Areas: ti
       break;
     default:
       return null;
@@ -33,3 +42,4 @@ const obtenerCargosArea = (rol) => {
 };
 
 module.exports = obtenerCargosArea;
+
