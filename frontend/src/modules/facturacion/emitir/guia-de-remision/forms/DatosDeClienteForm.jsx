8e3286ae054c3231384a91fe0682b868a6d31b05
Luis Saavedra Falcon <93019328+luisdevelop23@users.noreply.github.com>
2025-10-09 13:07:12 -0500

    campos editables guia

--- frontend/src/modules/facturacion/emitir/guia-de-remision/forms/DatosDeClienteForm.jsx
+++ frontend/src/modules/facturacion/emitir/guia-de-remision/forms/DatosDeClienteForm.jsx
@@ -158,7 +158,8 @@ const DatosDeClienteForm = () => {
                         id="cliente_Razon_Social"
                         name="cliente_Razon_Social"
                         value={cliente_Razon_Social}
-                        readOnly
+                        // readOnly
+                        onChange={handleChange}
                         className="px-3 py-2 block w-full rounded-md border text-gray-800 border-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 text-sm"
                     />
                 </div>
@@ -174,7 +175,8 @@ const DatosDeClienteForm = () => {
                         id="cliente_Direccion"
                         name="cliente_Direccion"
                         value={cliente_Direccion}
-                        readOnly
+                        // readOnly
+                        onChange={handleChange}
                         className="px-3 py-2 block w-full rounded-md border text-gray-800 border-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 text-sm"
                     />
                 </div>
