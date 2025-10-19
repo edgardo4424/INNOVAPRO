ec2496eec01dc0c827b920b43ce4e066eeafae13
devSimon20 <jeanpierks6@gmail.com>
2025-10-19 11:07:34 -0500

    feat: Se corrigio un bug en en calculo de contartos en la cts

--- backend/src/modules/cts/infraestructure/services/calcularContratosComputados.js
+++ backend/src/modules/cts/infraestructure/services/calcularContratosComputados.js
@@ -5,14 +5,13 @@ const calcularContratosComputados = (
    fechaFinRango,
    objetos
 ) => {
-   // console.log('Inicio de rango en el filtro: ',fechaInicioRango);
-   // console.log('Fin de rango en el filtro: ',fechaFinRango);
    
    
    //Retorna los contratos que se encuentran dentro del periodo de la cts
    let filtrados = objetos.filter((obj) => {
       const inicio = obj.fecha_inicio;
-      const fin = obj.fecha_fin;
+      const fin = obj.es_indefinido ? fechaFinRango : obj.fecha_fin;
+
 
       return (
          (inicio >= fechaInicioRango && inicio <= fechaFinRango) ||
