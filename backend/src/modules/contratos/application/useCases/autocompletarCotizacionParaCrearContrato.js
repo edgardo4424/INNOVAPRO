afc1b34d043348ef3771b3e321fad982ba47b82d
luisgl97 <luis.gonzales.ayvar@gmail.com>
2025-10-22 15:11:59 -0500

    (fix): Se añadieron los ids de relacion en el autocompletado de cotizacion

--- backend/src/modules/contratos/application/useCases/autocompletarCotizacionParaCrearContrato.js
+++ backend/src/modules/contratos/application/useCases/autocompletarCotizacionParaCrearContrato.js
@@ -17,26 +17,33 @@ module.exports = async (cotizacion_id, contratoRepository, transaction = null) =
     const respuesta = {
         id: cotizacion.id,
         obra: {
+            id: obra.id,
             nombre: obra.nombre,
             direccion: obra.direccion,
         },
         cliente: {
+            id: cliente.id,
             tipo: cliente.tipo,
             numero_documento: cliente.tipo == "Persona Natural" ? cliente.dni : cliente.ruc,
             nombre_cliente: cliente.razon_social, // asi sea Persona Natural o Jurídica
             domicilio_fiscal: cliente.domicilio_fiscal,
         },
         contacto: {
+            id: contacto.id,
             nombre: contacto.nombre,
             correo: contacto.email,
         },
         filial: {
+            id: empresas_proveedora.id,
             razon_social: empresas_proveedora.razon_social,
             ruc: empresas_proveedora.ruc,
             direccion: empresas_proveedora.direccion,
-
+            domicilio_representante_legal: empresas_proveedora.direccion_representante,
+            telefono: empresas_proveedora.telefono,
         },
         usuario: {
+
+            id: usuario.id,
             nombres: usuario.trabajador.nombres+" "+usuario.trabajador.apellidos,
             telefono: usuario.trabajador.telefono,
             correo: usuario.trabajador.correo,
@@ -46,6 +53,7 @@ module.exports = async (cotizacion_id, contratoRepository, transaction = null) =
             nombre: uso.descripcion,
         },
         cotizacion: {
+             id: informacionCotizacion.id,
               fecha: formatearFechaIsoADMY(informacionCotizacion.updatedAt),
               moneda: despiece.moneda,
               subtotal_con_descuento_sin_igv: despiece.subtotal_con_descuento,
@@ -59,17 +67,20 @@ module.exports = async (cotizacion_id, contratoRepository, transaction = null) =
 
         firmantes: {
             filial: {
-
+            id: empresas_proveedora.id,
             tipo_documento_representante_legal: empresas_proveedora.tipo_documento,
             numero_documento_representante_legal: empresas_proveedora.dni_representante,
             nombre_representante_legal: empresas_proveedora.representante_legal,
             cargo_representante_legal: empresas_proveedora.cargo_representante,
+           
             },
             cliente: {
+            id: cliente.id,
             tipo_documento_representante_legal: cliente.tipo_documento,
             numero_documento_representante_legal: cliente.tipo == "Persona Natural" ? cliente.dni : cliente.dni_representante,
             nombre_representante_legal: cliente.tipo == "Persona Natural" ? cliente.razon_social : cliente.representante_legal,
             cargo_representante_legal: cliente.cargo_representante,
+            
             }
         }
         }
